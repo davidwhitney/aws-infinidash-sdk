3d04ef40e0e27e8a990b90950b0bcf616d7d0fa5
David Whitney <david@davidwhitney.co.uk>
2021-07-02 17:38:54 +0100

    Init

---
+++ AWS.Infinidash.SDK/AWS.Infinidash.SDK/InfinidashContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Amazon.Infinidash.SDK
+{
+    public class InfinidashContext
+    {
+        // https://www.youtube.com/watch?v=x31tDT-4fQw
+        static InfinidashContext()
+        {
+            OpenBrowser("https://www.youtube.com/watch?v=x31tDT-4fQw");
+        }
+
+        public static void OpenBrowser(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+
+    public static class AppUseExtensions
+    {
+        public static IApplicationBuilder UseHttpsRedirection(this IApplicationBuilder app)
+        {
+
+        }
+    }
+}
